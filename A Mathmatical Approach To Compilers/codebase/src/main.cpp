9176e69cf6f2f82bb40cb4bf8ec504cb98a24de2
CamH04 <cameronahaynes04@gmail.com>
2025-07-31 00:59:10 +0100

    templete for codebase added

---
+++ A Mathmatical Approach To Compilers/codebase/src/main.cpp
@@ -0,0 +1,13 @@
+
+/***************************************************************************************
+*    Title: Tyto Compiler: For A Mathamatocle Approach To Compilers
+*    Author: Camerom Haynes
+*    Date: DD/MM/2025
+***************************************************************************************/
+
+#include<iostream>
+
+int main(){
+    std::cout << "=== Start Comp Of Tyto ===" << std::endl;
+    return 0;
+}
